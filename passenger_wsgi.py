7e5f97fd0bd50cc3221c4f37b7eebc757bed7642
Paweł Dudziński <pawel.dudzinski@saucelabs.com>
2019-07-08 19:54:10 +0200

    add passenger wsgi

---
+++ passenger_wsgi.py
@@ -0,0 +1,3 @@
+import sys, os
+sys.path.append(os.getcwd())
+from zaczepieni import app as application
--- zaczepieni/__init__.py
+++ zaczepieni/__init__.py
@@ -9,3 +9,6 @@ def main(global_config, **settings):
         config.include('.routes')
         config.scan()
     return config.make_wsgi_app()
+
+
+app = main({})
